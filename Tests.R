668a95c461b5bf9578f36de4d94fb2ee159ae9b2
leowangzexian <statistics.leo@gmail.com>
2024-11-13 01:41:45 -0600

    performed more tests on the functions in FunctionsNN.R in Tests.R

--- Tests.R
+++ Tests.R
@@ -2,10 +2,10 @@
 source("FunctionsNN.R")
 
 # first test case
-Y = c(0, 1, 2, 3, 4, 3, 2, 1, 0, 2, 3, 4, 1, 2, 0, 4)
-X = matrix(rnorm(16*19), 16)
-Yt = c(1, 0, 3, 2)
-Xt = matrix(rnorm(4*19), 4)
+Y = rbinom(100, size = 8, prob = 0.5) - 1
+X = matrix(rnorm(100*99, 1, 0.99), 100)
+Yt = rbinom(50, size = 8, prob = 0.5) - 1
+Xt = matrix(rnorm(50*99, 1, 0.99), 50)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 out2 = NN_train(X, Y, Xt, Yt, lambda = 0.001,
@@ -17,10 +17,10 @@ test_error = evaluate_error(Xt, Yt, out2$params$W1, out2$params$b1, out2$params$
 test_error
 
 # 2nd test case
-Y = rbinom(200, size = 10, prob = 0.5) - 1
-X = matrix(rbinom(200*99, 10, 0.5), 200)
-Yt = rbinom(30, size = 10, prob = 0.5) - 1
-Xt = matrix(rbinom(30*99, 10, 0.5), 30)
+Y = rbinom(200, size = 8, prob = 0.5) - 1
+X = matrix(rnorm(200*99, 1, 0.99), 200)
+Yt = rbinom(30, size = 8, prob = 0.5) - 1
+Xt = matrix(rnorm(30*99, 1, 0.99), 30)
 X = cbind(1, X)
 Xt = cbind(1, Xt)
 out2 = NN_train(X, Y, Xt, Yt, lambda = 0.001,
